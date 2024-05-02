a9a93c163f16454ca815dac52d481001aa982029
lucas <lucasvaugrente35@gmail.com>
2024-05-02 11:27:16 +0200

    Refactor script js

--- script.js
+++ script.js
@@ -5,116 +5,29 @@ const tooltip_rocket_text = document.querySelector('.tooltip-text')
 const footer = document.querySelector('footer');
 const launchButton = document.querySelector('.launchRocket');
 
-function animatePixel(pixelSmoke, rightMax) {
-    pixelSmoke.style.right = `${rightMax}px`;
-    pixelSmoke.style.backgroundColor = "rgba(114, 114, 114, 0.37)";
-    setTimeout(() => {
-        pixelSmoke.style.backgroundColor = "rgba(190, 190, 190, 0.192)";
-        setTimeout(() => {
-            pixelSmoke.style.backgroundColor = "transparent";
-            setTimeout(() => {
-                pixelSmoke.parentNode.removeChild(pixelSmoke);
-            }, 1000);
-        }, 1000);
-    }, 1000);
-    if (Math.random() < 0.5) {
-        setTimeout(function() {
-            pixelSmoke.style.bottom = Math.floor(Math.random() * 51) + 300 + "px";
-        }, 500);
-    }
-    setTimeout(function() {
-        // pixelSmoke.parentNode.removeChild(pixelSmoke);
-    }, 3000);
-}
 
-function generateSmoke() {
-    for (let index = 0; index < 1000; index++) {
-        const smokeRocket = document.querySelector(".smokeRocket");
-        const blockSmoke = document.createElement("div");
-        blockSmoke.classList.add("pixelSmoke");
-    
-        const bottom = Math.floor(Math.random() * 51) + 250;
-        const rightMin = 1000;
-        const width_height = Math.floor(Math.random() * 11) + 5;
-
-        blockSmoke.style.right = `${rightMin}px`;
-        blockSmoke.style.bottom = `${bottom}px`;
-        blockSmoke.style.width = `${width_height}px`;
-        blockSmoke.style.height = `${width_height}px`;
-    
-        smokeRocket.appendChild(blockSmoke);
-
-        let randomNumber;
-        if (Math.random() < 0.5) {
-            randomNumber = Math.floor(Math.random() * 401) + 1000;
-        } else {
-            randomNumber = Math.floor(Math.random() * 401) + 600;
-        }
-        
-        const rightMax = randomNumber;
-        const delay = Math.floor(Math.random() * 1001) + 1000;
-        setTimeout(() => {
-            blockSmoke.style.opacity = "1";
-            animatePixel(blockSmoke, rightMax);
-        }, delay);
-    }
-}
-
-// generateSmoke()
-
-const landing = false;
-
-const maxTop = window.innerHeight - footer.offsetHeight / 1.5;
-
+const maxTop = window.innerHeight - footer.offsetHeight / 2;
+const numberSmoke = 200;
 const starsNumber = 70;
 let scroll_count = 0;
-const lineHeader = 10;
-const lineFooter = 90;
 
-// Stars generation (random positions)
-for (let i = 0; i < starsNumber; i++) {
-    const star = document.createElement('div');
-    star.classList.add('mini-star');
-    star.style.left = Math.random() * 98 + 'vw';
-    star.style.top = Math.random() * document.documentElement.scrollHeight + 'px';
+const textTooltipBegin = "Vous pouvez me suivre tout au long du périple en me survolant :)";
+let indexLetter = 0;
 
-    const widthANDheight = Math.random() * 5 + 1;
-    star.style.width = widthANDheight + 'px';
-    star.style.height = widthANDheight + 'px';
+const slow = 10000;
+const effect = easeInOutCuaic;
 
-    body.appendChild(star);
-}
+const rocketFrameImages = [];
+const numberRocketFrames = 10;
 
-let stars = document.querySelectorAll('.mini-star');
-for (let index = 0; index < 20; index++) {
-    let star = stars[Math.floor(Math.random() * stars.length)];
-    star.style.animation = "twinkle 3s ease-in-out infinite";
-}
-
-const rocketFrameImages = [
-    'img/rocket/rocket1.png',
-    'img/rocket/rocket2.png',
-    'img/rocket/rocket3.png',
-    'img/rocket/rocket4.png',
-    'img/rocket/rocket5.png',
-    'img/rocket/rocket6.png',
-    'img/rocket/rocket7.png',
-    'img/rocket/rocket8.png',
-    'img/rocket/rocket9.png',
-    'img/rocket/rocket10.png'
-];
-
-
-function preloadImages(urls) {
-    urls.forEach(url => {
-        const img = new Image();
-        img.src = url;
-    });
+for (let i = 1; i <= numberRocketFrames; i++) {
+    rocketFrameImages.push(`img/rocket/rocket${i}.png`);
 }
 
 preloadImages(rocketFrameImages);
+updateFrames(true);
 
-updateFrames(true)
+generateStars();
 
 document.addEventListener("scroll", function () {
     const scrollPercentage = getScrollPercentage();
@@ -126,11 +39,9 @@ document.addEventListener("scroll", function () {
         rocket.style.backgroundImage = `url('img/rocket/rocket.png')`;
         rocket.classList.remove('animation');
         updateFrames(false);
-        launchButton.classList.add("visible")
     } else {
         rocket.style.filter = "drop-shadow(0 40px 15px rgb(255, 240, 25))";
         rocket.classList.add('animation');
-        launchButton.classList.remove("visible")
         updateFrames(true); // Activer l'animation lorsque vous n'êtes pas en bas de la page
     }
 
@@ -139,6 +50,72 @@ document.addEventListener("scroll", function () {
 
     scroll_count = scrollPercentage;
 });
+
+rocket.addEventListener('mouseenter', () => {
+    tooltip_rocket.style.opacity = "1";
+    animateText()
+});
+
+rocket.addEventListener('mouseleave', () => {
+    tooltip_rocket.style.opacity = "";
+    eraseText()
+    indexLetter = 0;
+});
+
+launchButton.addEventListener('click', () => {
+    const titleWelcome = document.querySelector('header h1');
+    const subtitleWelcome = document.querySelector('header p');
+    titleWelcome.innerHTML = "Oh... are you back ? 👀";
+    subtitleWelcome.innerHTML = "Thank you for visiting my page 🤗";
+    document.documentElement.classList.add('smooth-scroll');
+    
+    generateSmoke();
+    slowScrollToTop();
+});
+
+if(detectMobile()) {
+    const titlesDrawing = document.querySelectorAll('.drawing h3');
+    titlesDrawing.forEach(function (titleDrawing, currentIndex) {
+        titleDrawing.style.opacity = '1';
+    }, "myThisArg");
+}
+
+/* ######################################################################################################### */
+/* ##############################################  FONCTIONS  ############################################## */
+/* ######################################################################################################### */
+
+function preloadImages(urls) {
+    urls.forEach(url => {
+        const img = new Image();
+        img.src = url;
+    });
+}
+
+/**
+ * 
+ * @summary Generate stars on the background
+ *
+ */
+function generateStars() {
+    for (let i = 0; i < starsNumber; i++) {
+        const star = document.createElement('div');
+        star.classList.add('mini-star');
+        star.style.left = Math.random() * 98 + 'vw';
+        star.style.top = Math.random() * document.documentElement.scrollHeight + 'px';
+
+        const widthANDheight = Math.random() * 5 + 1;
+        star.style.width = widthANDheight + 'px';
+        star.style.height = widthANDheight + 'px';
+
+        body.appendChild(star);
+    }
+
+    let stars = document.querySelectorAll('.mini-star');
+    for (let index = 0; index < 20; index++) {
+        let star = stars[Math.floor(Math.random() * stars.length)];
+        star.style.animation = "twinkle 3s ease-in-out infinite";
+    }
+}
 
 /**
  * 
@@ -165,7 +142,7 @@ function getScrollPercentage() {
 
 /**
  * 
- * @summary Met à jour les frames de la fusée
+ * @summary Updates rocket frames
  * 
  */ 
 function updateFrames(active) {
@@ -174,7 +151,6 @@ function updateFrames(active) {
             let index = Math.floor(Math.random() * rocketFrameImages.length);
             rocket.style.backgroundImage = `url('${rocketFrameImages[index]}')`;
         }
-        // Vous pouvez également vérifier si l'intervalle est déjà en cours pour éviter de le réinitialiser à chaque fois.
         if (!updateFrames.intervalId) {
             updateFrames.intervalId = setInterval(updateBackground, 100);
         }
@@ -183,10 +159,6 @@ function updateFrames(active) {
         updateFrames.intervalId = null;
     }
 }
-
-const textTooltipBegin = "Vous pouvez me suivre tout au long du périple en me survolant :)";
-
-let indexLetter = 0;
 
 function animateText() {
     if (indexLetter < textTooltipBegin.length) {
@@ -200,57 +172,67 @@ function eraseText() {
     tooltip_rocket_text.textContent = ""
 }
 
-rocket.addEventListener('mouseenter', () => {
-    tooltip_rocket.style.opacity = "1";
-    animateText()
-});
-
-rocket.addEventListener('mouseleave', () => {
-    tooltip_rocket.style.opacity = "";
-    eraseText()
-    indexLetter = 0;
-});
-
-/**
- * 
- * @summary Détecte si l'utilisateur est sur un appareil mobile
- * 
- */ 
-function detectMobile() {
-    const toMatch = [
-        /Android/i,
-        /webOS/i,
-        /iPhone/i,
-        /iPad/i,
-        /iPod/i,
-        /BlackBerry/i,
-        /Windows Phone/i
-    ];
+function animatePixel(pixelSmoke, rightMax, goToRight) {
+    let delay = 4000;
+    pixelSmoke.style.right = `${rightMax}px`;
+    pixelSmoke.style.backgroundColor = "rgba(114, 114, 114, 0.37)";
+    setTimeout(() => {
+        pixelSmoke.style.backgroundColor = "rgba(190, 190, 190, 0.192)";
+        setTimeout(() => {
+            pixelSmoke.style.backgroundColor = "transparent";
+        }, 1000);
+    }, 1000);
+    if (Math.random() < 0.7) {
+        setTimeout(function() {
+            if (rightMax > 250) {
+                pixelSmoke.style.bottom = Math.floor(Math.random() * 101) + 200 + "px";
+            }
+        }, 500);
+    }
+    if ((goToRight && rightMax > window.innerWidth - 5) || pixelSmoke.getBoundingClientRect().bottom > window.innerHeight) {
+        pixelSmoke.parentNode.removeChild(pixelSmoke);
+        return;
+    }
     
-    return toMatch.some((toMatchItem) => {
-        return navigator.userAgent.match(toMatchItem);
-    });
+    setTimeout(function() {
+        pixelSmoke.parentNode.removeChild(pixelSmoke);
+    }, delay);
 }
 
-if(detectMobile()) {
-    const titlesDrawing = document.querySelectorAll('.drawing h3');
-    titlesDrawing.forEach(function (titleDrawing, currentIndex) {
-        titleDrawing.style.opacity = '1';
-    }, "myThisArg");
+function generateSmoke() {
+    for (let index = 0; index < numberSmoke; index++) {
+        const smokeRocket = document.querySelector(".smokeRocket");
+        const blockSmoke = document.createElement("div");
+        blockSmoke.classList.add("pixelSmoke");
+    
+        const bottom = Math.floor(Math.random() * 51) + 150;
+        const rightMin = 70;
+        const width_height = Math.floor(Math.random() * 7) + 2;
+
+        blockSmoke.style.right = `${rightMin}px`;
+        blockSmoke.style.bottom = `${bottom}px`;
+        blockSmoke.style.width = `${width_height}px`;
+        blockSmoke.style.height = `${width_height}px`;
+    
+        smokeRocket.appendChild(blockSmoke);
+
+        let randomNumber;
+        let goToRight = false;
+        if (Math.random() < 0.8) {
+            randomNumber = Math.floor(Math.random() * 401) + rightMin;
+        } else {
+            randomNumber = Math.floor(Math.random() * rightMin);
+            goToRight = true;
+        }
+        
+        const rightMax = randomNumber;
+        const delay = Math.floor(Math.random() * 1001) + 1000;
+        setTimeout(() => {
+            blockSmoke.style.opacity = "1";
+            animatePixel(blockSmoke, rightMax, goToRight);
+        }, delay);
+    }
 }
-
-launchButton.addEventListener('click', () => {
-    const titleWelcome = document.querySelector('header h1');
-    const subtitleWelcome = document.querySelector('header p');
-    titleWelcome.innerHTML = "Oh... are you back ? 👀";
-    subtitleWelcome.innerHTML = "Thank you for visiting my page 🤗";
-    document.documentElement.classList.add('smooth-scroll');
-
-    slowScrollToTop();
-});
-
-const slow = 10000;
-const effect = easeInOutCuaic;
 
 function slowScrollToTop() {
     const e = document.documentElement;
@@ -260,7 +242,6 @@ function slowScrollToTop() {
     }
 }
 
-// Element to move + duration in milliseconds
 function scrollTo(element, duration) {
     var e = document.documentElement;
     if (e.scrollTop === 0) {
@@ -271,7 +252,6 @@ function scrollTo(element, duration) {
     scrollToC(e, e.scrollTop, element, duration);
 }
 
-// Element to move, element or px from, element or px to, time in ms to animate
 function scrollToC(element, from, to, duration) {
     if (duration <= 0) return;
     if (typeof from === "object") from = from.offsetTop;
@@ -292,10 +272,31 @@ function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
     }, step);
 }
 
-
 function easeInOutCuaic(t) {
     t /= 0.5;
     if (t < 1) return t * t * t / 2;
     t -= 2;
     return (t * t * t + 2) / 2;
-  }
+}
+
+/* ################ DETECT ON MOBILE ################ */
+/**
+ * 
+ * @summary Détecte si l'utilisateur est sur un appareil mobile
+ * 
+ */ 
+function detectMobile() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
